fe76b3cfc72e0c75449360ad19f9f9d270a7a4cb
KryptKode <paulakhamiogu@gmail.com>
2017-06-08 12:30:00 -0700

    Added SharedPreferences, programmatically update spinner items from shared preferences

--- app/src/main/java/com/kryptkode/cyberman/eritsmartdisplay/EritSmartDisplayActivity.java
+++ app/src/main/java/com/kryptkode/cyberman/eritsmartdisplay/EritSmartDisplayActivity.java
@@ -1,6 +1,11 @@
 package com.kryptkode.cyberman.eritsmartdisplay;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,7 +14,19 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class EritSmartDisplayActivity extends AppCompatActivity {
+import com.kryptkode.cyberman.eritsmartdisplay.fragments.EritSmartDisplayFragment;
+
+import static com.kryptkode.cyberman.eritsmartdisplay.R.id.quizFragment;
+
+public class EritSmartDisplayActivity extends AppCompatActivity implements EritSmartDisplayFragment.SpinnerEntriesChangeListener{
+    public static final String BRT_KEY = "brt";
+    public static final String SPD_KEY = "spd";
+    public static final String INV_KEY = "inv";
+    public static final String NUM_OF_MSGS_KEY = "number_of_messages";
+    public static final String SERIAL_KEY = "serial";
+
+    private boolean phoneDevice = true; // to check if the device is a phone in order to set the layout for portrait mode
+    private boolean preferencesChanged = true; //to check if the preference changed in order to update the UI with the current setting
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +34,23 @@ public class EritSmartDisplayActivity extends AppCompatActivity {
         setContentView(R.layout.activity_erit_smart_display);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        //set the default values in the shared preferences
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+
+        //register the listener for the SharedPreferences changes
+        PreferenceManager.getDefaultSharedPreferences(this).
+                registerOnSharedPreferenceChangeListener(preferencesChangeListener);
+
+        //find the screen size
+        int screenSize = getResources().getConfiguration().
+                screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK;
+
+        //if the device is a tablet, set the phoneDevice variable to false
+        if (screenSize == Configuration.SCREENLAYOUT_SIZE_LARGE ||
+                screenSize == Configuration.SCREENLAYOUT_SIZE_XLARGE)
+            phoneDevice = false;
 
        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -28,10 +62,32 @@ public class EritSmartDisplayActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        if (preferencesChanged){
+            //since the default preferences have been set,
+            //initialize the MainActivityFragment and start the quiz
+            EritSmartDisplayFragment displayFragment =
+                    (EritSmartDisplayFragment) getSupportFragmentManager().findFragmentById(
+                            R.id.display_fragment);
+            displayFragment.displayText(PreferenceManager.getDefaultSharedPreferences(this));
+            displayFragment.setNum(Integer.parseInt(PreferenceManager.getDefaultSharedPreferences(this).getString(NUM_OF_MSGS_KEY, null)));
+            displayFragment.addSpinnerEntries();
+            preferencesChanged = false;
+        }
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_erit_smart_display, menu);
-        return true;
+
+        if (phoneDevice){
+            getMenuInflater().inflate(R.menu.menu_erit_smart_display, menu);
+            return true;
+        }
+        else{
+            return  false;
+        }
+
     }
 
     @Override
@@ -41,7 +97,8 @@ public class EritSmartDisplayActivity extends AppCompatActivity {
         switch (id){
 
             case R.id.action_settings:
-                //TODO: Add action
+                Intent settingsIntent = new Intent (this , SettingsActivity.class);
+                startActivity(settingsIntent);
                 break;
             case R.id.action_sync:
                 //TODO: Add action
@@ -54,5 +111,38 @@ public class EritSmartDisplayActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private SharedPreferences.OnSharedPreferenceChangeListener preferencesChangeListener =
+            new SharedPreferences.OnSharedPreferenceChangeListener() {
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            preferencesChanged = true; //true because the user has changed the settings
+
+            EritSmartDisplayFragment displayFragment =
+                    (EritSmartDisplayFragment) getSupportFragmentManager().findFragmentById(
+                            R.id.display_fragment);
+
+            if (key.equals(BRT_KEY)){
+                //do something
+
+            }
+            if (key.equals(SPD_KEY)){
+                //do something
+            }
+            if (key.equals(INV_KEY)){
+                //do something
+
+            }
+            if (key.equals(NUM_OF_MSGS_KEY)){
+                //do something
+                displayFragment.setNum(Integer.parseInt(sharedPreferences.getString(NUM_OF_MSGS_KEY, null)));
+            }
+        }
+    };
+
+    @Override
+    public void addSpinnerEntries() {
+
     }
 }
--- app/src/main/java/com/kryptkode/cyberman/eritsmartdisplay/fragments/EritSmartDisplayFragment.java
+++ app/src/main/java/com/kryptkode/cyberman/eritsmartdisplay/fragments/EritSmartDisplayFragment.java
@@ -1,29 +1,110 @@
 package com.kryptkode.cyberman.eritsmartdisplay.fragments;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.kryptkode.cyberman.eritsmartdisplay.EritSmartDisplayActivity;
 import com.kryptkode.cyberman.eritsmartdisplay.R;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class EritSmartDisplayFragment extends Fragment {
+
+    private TextView textView;
+    private Spinner spinner;
+    private EditText enterMessageEditText;
+    private EditText pmsThreeDigitEditText;
+    private EditText dpkThreeDigitEditText;
+    private EditText agoThreeDigitEditText;
+    private EditText pmsTwoDigitEditText;
+    private EditText dpkTwoDigitEditText;
+    private EditText agoTwoDigitEditText;
+
+    private SpinnerEntriesChangeListener listener;
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    private int num;
 
     public EritSmartDisplayFragment() {
         // Required empty public constructor
     }
 
+    public interface SpinnerEntriesChangeListener{
+        void addSpinnerEntries();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_erit_smart_display, container, false);
+        View view = inflater.inflate(R.layout.fragment_erit_smart_display, container, false);
+
+        //instantiate the views
+        textView = (TextView) view.findViewById(R.id.test);
+
+        enterMessageEditText = (EditText) view.findViewById(R.id.edit_enter_message);
+
+        pmsThreeDigitEditText = (EditText) view.findViewById(R.id.pms_000);
+        pmsTwoDigitEditText = (EditText) view.findViewById(R.id.pms_00);
+        dpkThreeDigitEditText = (EditText) view.findViewById(R.id.dpk_000);
+        dpkTwoDigitEditText = (EditText) view.findViewById(R.id.dpk_00);
+        agoTwoDigitEditText = (EditText) view.findViewById(R.id.ago_000);
+        agoThreeDigitEditText = (EditText) view.findViewById(R.id.ago_00);
+
+        spinner = (Spinner) view.findViewById(R.id.spinner);
+        //listener.addSpinnerEntries();
+
+        return  view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    public void addSpinnerEntries() {
+        Log.v("SPINNER", "Num_of_msg-->" + num);
+        List<String> spinnerEntries = new ArrayList<>();
+        for (int i = 1; i <= num; i++){
+            spinnerEntries.add(getString(R.string.message_x, i));
+        }
+        Log.v("SPINNER", "Array-->" + spinnerEntries.size());
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                getContext(), android.R.layout.simple_spinner_item, spinnerEntries
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+    public void displayText(SharedPreferences preferences){
+        textView.append("BRT-->" +String.valueOf(preferences.getInt(EritSmartDisplayActivity.BRT_KEY, 0)) + "\n");
+        textView.append("SPD-->" +String.valueOf(preferences.getInt(EritSmartDisplayActivity.SPD_KEY, 0)) + "\n");
+        textView.append("INV-->" +String.valueOf(preferences.getString(EritSmartDisplayActivity.INV_KEY, null)) + "\n");
+        textView.append("NUM-->" +String.valueOf(preferences.getString(EritSmartDisplayActivity.NUM_OF_MSGS_KEY, null)) + "\n");
+        num = Integer.parseInt(preferences.getString(EritSmartDisplayActivity.NUM_OF_MSGS_KEY, null));
+        Log.v("SPINNER", "Num_of_msgs-->" + num);
     }
 
 
